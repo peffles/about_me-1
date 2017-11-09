f360ad62ceeeb7a9da308b782055587aaf8e2d8e
Kowser Kassa <kowserk7@gmail.com>
2017-11-08 18:54:24 -0800

    Finished the assignment

--- about-me/app.js
+++ about-me/app.js
@@ -1,6 +1,6 @@
 'user strict';
 
-
+var score = 0;
 
 var Name = prompt ('Please state your name');
 console.log('name', Name);
@@ -15,6 +15,7 @@ console.log('languages', Language);
 if (Language === 'yes' || Language === 'y') {
   alert ('That is correct, I speake Arabic, English and Tigrinia.');
   console.log('true', Language);
+  score++;
 }
 else if (Language === 'no' || Language === 'n') {
   alert ('I actually do speak 3 languages. I speak Arabic, English, and Tigrinia');
@@ -27,6 +28,7 @@ console.log('sibilings', Sibilings);
 if (Sibilings === 'no' || Sibilings === 'n') {
   alert ('You\'re right '+Name+', I have 7 sisters and 1 brother.');
   console.log('true', Sibilings);
+  score++;
 } else if (Sibilings === 'yes' || Sibilings === 'y') {
   alert ('I\'m sorry, that\'s wrong '+Name+'. I have 7 sisters and 1 brother.');
   console.log('false', Sibilings);
@@ -37,6 +39,7 @@ console.log('birthday', Birthday);
 if (Birthday === 'yes' || Birthday === 'y') {
   alert ('It really is!');
   console.log('true', Birthday);
+  score++;
 } else if (Birthday === 'no' || Birthday === 'n') {
   alert ('It actually is my birthday!');
   console.log('false', Birthday);
@@ -48,6 +51,7 @@ console.log('birthplace', BirthPlace);
 if (BirthPlace === 'yes' || BirthPlace === 'y') {
   alert ('Sure is! Born in the ole U S of A!');
   console.log('true', BirthPlace);
+  score++;
 } else if (BirthPlace === 'no' || BirthPlace === 'n') {
   alert ('AWW, that was incorrect '+Name+'. I was born right here in Seattle, WA.');
   console.log('false', BirthPlace);
@@ -58,16 +62,20 @@ var Color = prompt ('Is my favorite color orange?').toLowerCase();
 console.log('color', Color);
 if (Color === 'no' || Color === 'n') {
   alert('You\'re right '+Name+'! My favorite color is GREEN.');
+  console.log('true', Color);
+  score++;
 } else if (Color === 'yes' || Color === 'y') {
   alert('No '+Name+', my favorite color is GREEN');
-  console.log('true', Color);
+  console.log('false', Color);
 }
 
 
 var Numbers = prompt ('Can you guess my favorite number? You get 4 guesses!');
 if (Numbers === parseInt(Numbers, 7)) {
   alert ('You guessed right!');
+  score++;
   console.log('true', Numbers);
+  score++;
 }
 var counter = 1;
 while (Numbers !== '7' && Numbers !== null && counter !== 4) {
@@ -84,3 +92,23 @@ while (Numbers !== '7' && Numbers !== null && counter !== 4) {
 }
 console.log('counter change', counter);
 alert ('The correct answer is LUCKY 7!');
+
+var Country = ['ethiopia','germany','egypt','britain'];
+console.log('counrty', Country);
+for (var i=0; i<6; i++) {
+  var CountryInput =  prompt ('I have visited four different countries. Can you guess one of them?').toLowerCase();
+  if (Country.includes(CountryInput)) {
+    alert ('You guessed right!');
+    console.log('true', Country);
+    score++;
+    break;
+  } else {
+    alert ('I\'ve never been there. Please try again.');
+    console.log('false', Country);
+    while (i===5) {
+      alert('You didn\'t get any right :(. The correct countries are Ethiopia, Germany, Egypt and Britain. Thank you for playing');
+      break;
+    }
+  }
+}
+alert('Congratulations '+Name+', you got '+score+' out of 7!');
